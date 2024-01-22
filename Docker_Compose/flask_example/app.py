fa34d5f96384e0fc8fed788915fa349fd3ed7293
Richard Hawkins <Richard.Hawkins@Revature.com>
2024-01-22 13:43:49 -0600

    update with the days example

--- Docker_Compose/flask_example/app.py
+++ Docker_Compose/flask_example/app.py
@@ -3,11 +3,22 @@ from flask import Flask
 app = Flask(__name__)
 count = 0
 
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
 @app.route("/")
 def hello_world():
     global count
     count += 1
     return f"<h1>Starter Flask App</h1>{count}"
+
+@app.route("/kill")
+def kill():
+    shutdown_server()
+    return "shutting down"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
